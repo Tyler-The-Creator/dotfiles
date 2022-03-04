01eb412f8bd34cd2ebbee15b1f59a4fadb074a27
Tyler Pillay <treshpillay@gmail.com>
2022-03-04 21:14:14 +0200

    update brew install command

--- install/brew.sh
+++ install/brew.sh
@@ -2,7 +2,7 @@
 
 if test ! $(which brew); then
   installing "Homebrew"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 function install_or_upgrade {
